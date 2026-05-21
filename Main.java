673d643f8b5a48da1c669e77c99a2d4c1e80508d
Guineapig12343 <567557@my.cuhsd.org>
2026-05-21 18:32:33 +0000

    test subject bro

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Main {
+    
+
+    void main() {
+    System.out.println("hello world");
+    JOptionPane.showConfirmDialog(null,"hey there");
+    String name = JOptionPane.showInputDialog("what name sya it now bro");
+    }
+}
