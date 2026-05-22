38d1fecd99d660fe25a183c581f6cff14835e250
Guineapig12343 <567557@my.cuhsd.org>
2026-05-22 15:28:47 +0000

    woah tetst

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiDeviceReceiver;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.*;
 import javax.swing.JOptionPane;
 
 
 public class Main {
     
 
-    void main() {
-    System.out.println("hello world");
-    JOptionPane.showConfirmDialog(null,"hey there");
-    String name = JOptionPane.showInputDialog("what name sya it now bro");
-    System.out.println("whats up "+name);
+    public static void main(String[] args) {
+        
     
+    System.out.println("hello world");
+    /*JOptionPane.showConfirmDialog(null,"hey there");
+    String name = JOptionPane.showInputDialog("what name sya it now bro");
+    System.out.println("whats up "+name);*/
+    MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+    System.out.println(infos);
+
+    int getMaxTransmitters;
+    System.out.println(getMaxTransmitters);
     }
 }
